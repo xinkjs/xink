926a86b977efe7987417489c45ca09cc3573d2d7
Jason Creviston <the@j4w8n.com>
2025-03-30 17:33:28 -0400

    chore(cli): remove files after history merge

--- bin.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import pkg from './package.json' with { type: 'json' }
-import { program } from 'commander'
-import { create } from './commands/create.ts'
-
-program.name(pkg.name).version(pkg.version, '-v, --version')
-program.addCommand(create)
-program.parse()
